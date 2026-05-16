91313d5831b5e0f20c6bce0644e58c09172d14a0
Лисенко Софія <146013576+Yosohanomi@users.noreply.github.com>
2026-05-16 23:13:30 +0300

    main page desktop

--- src/entities/IncomeTable/IncomeTable.jsx
+++ src/entities/IncomeTable/IncomeTable.jsx
@@ -4,48 +4,123 @@ import styles from './IncomeTable.module.scss'
 export default function IncomeTable() {
   return (
     <div className={styles['income-table']}>
-      <table className={styles['income-table__table']}>
-        <thead className={styles['income-table__header']}>
-          <tr className={styles['income-table__header-row']}>
-            <th className={styles['income-table__header-cell']}>ДАТА</th>
-            <th className={styles['income-table__header-cell']}>ОПИС</th>
-            <th className={styles['income-table__header-cell']}>КАТЕГОРІЯ</th>
-            <th className={styles['income-table__header-cell']}>СУМА</th>
-          </tr>
-        </thead>
-        <tbody className={styles['income-table__body']}>
-          <tr className={styles['income-table__row']}>
-            <td className={styles['income-table__cell']}>05.09.2019</td>
-            <td className={styles['income-table__cell']}>Моя зп</td>
-            <td className={styles['income-table__cell']}>зп</td>
-            <td className={styles['income-table__cell']}>
-              <div className={styles['income-table__amount-wrapper']}>
-                <span className={styles['income-table__amount']}>20 000.00 грн</span>
-                <button className={styles['income-table__delete-button']}>
-                  <svg className={styles['income-table__delete-icon']}>
-                    <use href={`${SVGIcon}#icon-trashbin`} />
-                  </svg>
-                </button>
-              </div>
-            </td>
-          </tr>
-          <tr className={styles['income-table__row']}>
-            <td className={styles['income-table__cell']}>05.09.2019</td>
-            <td className={styles['income-table__cell']}>% на залишок на карті</td>
-            <td className={styles['income-table__cell']}>Дод. прибуток</td>
-            <td className={styles['income-table__cell']}>
-              <div className={styles['income-table__amount-wrapper']}>
-                <span className={styles['income-table__amount']}>500.00 грн</span>
-                <button className={styles['income-table__delete-button']}>
-                  <svg className={styles['income-table__delete-icon']}>
-                    <use href={`${SVGIcon}#icon-trashbin`} />
-                  </svg>
-                </button>
-              </div>
-            </td>
-          </tr>
-        </tbody>
-      </table>
+      <div className={styles['income-table__thumb']}>
+          <table className={styles['income-table__table']}>
+            <thead className={styles['income-table__header']}>
+              <tr className={styles['income-table__header-row']}>
+                <th className={styles['income-table__header-cell']}>ДАТА</th>
+                <th className={styles['income-table__header-cell']}>ОПИС</th>
+                <th className={styles['income-table__header-cell']}>КАТЕГОРІЯ</th>
+                <th className={styles['income-table__header-cell']}>СУМА</th>
+              </tr>
+            </thead>
+            <tbody className={styles['income-table__body']}>
+              <tr className={styles['income-table__row']}>
+                <td className={styles['income-table__cell']}>05.09.2019</td>
+                <td className={styles['income-table__cell']}>Моя зп</td>
+                <td className={styles['income-table__cell']}>зп</td>
+                <td className={styles['income-table__cell']}>
+                  <div className={styles['income-table__amount-wrapper']}>
+                    <span className={styles['income-table__amount']}>20 000.00 грн</span>
+                    <button className={styles['income-table__delete-button']}>
+                      <svg className={styles['income-table__delete-icon']}>
+                        <use href={`${SVGIcon}#icon-trashbin`} />
+                      </svg>
+                    </button>
+                  </div>
+                </td>
+              </tr>
+              <tr className={styles['income-table__row']}>
+                <td className={styles['income-table__cell']}>05.09.2019</td>
+                <td className={styles['income-table__cell']}>% на залишок на карті</td>
+                <td className={styles['income-table__cell']}>Дод. прибуток</td>
+                <td className={styles['income-table__cell']}>
+                  <div className={styles['income-table__amount-wrapper']}>
+                    <span className={styles['income-table__amount']}>500.00 грн</span>
+                    <button className={styles['income-table__delete-button']}>
+                      <svg className={styles['income-table__delete-icon']}>
+                        <use href={`${SVGIcon}#icon-trashbin`} />
+                      </svg>
+                    </button>
+                  </div>
+                </td>
+              </tr>
+
+              <tr className={styles['income-table__row']}>
+                <td className={styles['income-table__cell']}></td>
+                <td className={styles['income-table__cell']}></td>
+                <td className={styles['income-table__cell']}></td>
+                <td className={styles['income-table__cell']}>
+                </td>
+              </tr>
+
+              <tr className={styles['income-table__row']}>
+                <td className={styles['income-table__cell']}></td>
+                <td className={styles['income-table__cell']}></td>
+                <td className={styles['income-table__cell']}></td>
+                <td className={styles['income-table__cell']}>
+                </td>
+              </tr>
+
+              <tr className={styles['income-table__row']}>
+                <td className={styles['income-table__cell']}></td>
+                <td className={styles['income-table__cell']}></td>
+                <td className={styles['income-table__cell']}></td>
+                <td className={styles['income-table__cell']}>
+                </td>
+              </tr>
+
+              <tr className={styles['income-table__row']}>
+                <td className={styles['income-table__cell']}></td>
+                <td className={styles['income-table__cell']}></td>
+                <td className={styles['income-table__cell']}></td>
+                <td className={styles['income-table__cell']}>
+                </td>
+              </tr>
+
+              <tr className={styles['income-table__row']}>
+                <td className={styles['income-table__cell']}></td>
+                <td className={styles['income-table__cell']}></td>
+                <td className={styles['income-table__cell']}></td>
+                <td className={styles['income-table__cell']}>
+                </td>
+              </tr>
+
+              <tr className={styles['income-table__row']}>
+                <td className={styles['income-table__cell']}></td>
+                <td className={styles['income-table__cell']}></td>
+                <td className={styles['income-table__cell']}></td>
+                <td className={styles['income-table__cell']}>
+                </td>
+              </tr>
+
+              <tr className={styles['income-table__row']}>
+                <td className={styles['income-table__cell']}></td>
+                <td className={styles['income-table__cell']}></td>
+                <td className={styles['income-table__cell']}></td>
+                <td className={styles['income-table__cell']}>
+                </td>
+              </tr>
+
+              <tr className={styles['income-table__row']}>
+                <td className={styles['income-table__cell']}></td>
+                <td className={styles['income-table__cell']}></td>
+                <td className={styles['income-table__cell']}></td>
+                <td className={styles['income-table__cell']}>
+                </td>
+              </tr>
+
+              <tr className={styles['income-table__row']}>
+                <td className={styles['income-table__cell']}></td>
+                <td className={styles['income-table__cell']}></td>
+                <td className={styles['income-table__cell']}></td>
+                <td className={styles['income-table__cell']}>
+                </td>
+              </tr>
+            </tbody>
+          </table>
+      </div>
+      
       
       <div className={styles['income-table__summary']}>
         <h4 className={styles['income-table__summary-title']}>Зведення</h4>
